41b14ddf500f41c20a29a39977895a276cf96f51
Mike Dillamore <mike@dillamore.com>
2024-01-12 16:51:40 +0000

    Obtain and store plant ID

--- action_helpers/get_plant_ids.py
+++ action_helpers/get_plant_ids.py
@@ -1,7 +1,5 @@
 import os
 import growattServer
-import json
-import datetime
 
 username = os.environ.get('SHINEPHONE_USERNAME', '')
 password = os.environ.get('SHINEPHONE_PASSWORD', '')
@@ -10,18 +8,10 @@ api = growattServer.GrowattApi()
 login_response = api.login(username, password)
 
 if login_response['success']:
-    #Get a list of plants
     user_id = login_response['userId']
-    #print(user_id)
     plant_list = api.plant_list(user_id)
-    formatted_plant = json.dumps(plant_list, indent=2)
-    print(formatted_plant)
 
     #Get ID of first plant
+    #TODO - get all plant IDs as a list
     plant_id = plant_list['data'][0]['plantId']
-    #print(plant_id)
-
-    now = datetime.datetime.now()
-    data = api.plant_detail(plant_id, growattServer.Timespan.day, now)
-    formatted_data = json.dumps(data, indent=2, sort_keys=True)
-    print(formatted_data)
+    print(plant_id)

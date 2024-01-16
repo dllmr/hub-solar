55f793aca19efa9abd0129ecb63f39c4ea848384
Mike Dillamore <mike@dillamore.com>
2024-01-16 17:36:14 +0000

    Python script now creates config file

---
+++ action_helpers/default_config.py
@@ -0,0 +1,19 @@
+import os
+import json
+import growattServer
+
+username = os.environ.get('SHINEPHONE_USERNAME', '')
+password = os.environ.get('SHINEPHONE_PASSWORD', '')
+
+api = growattServer.GrowattApi()
+login_response = api.login(username, password)
+
+if login_response['success']:
+    config = {
+        'userId': login_response['userId'],
+        'plantId': login_response['data'][0]['plantId'],
+        'plantName': login_response['data'][0]['plantName']
+    }
+
+    with open('../plant_data/config.json', 'w') as config_file:
+        json.dump(config, config_file, indent=2)

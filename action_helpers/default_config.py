0dff933663af61e25ca7b25c2e031af8fd723df5
Mike Dillamore <mike@dillamore.com>
2024-01-17 09:59:52 +0000

    Add start date to config; move config to own folder

--- action_helpers/default_config.py
+++ action_helpers/default_config.py
@@ -1,6 +1,8 @@
 import os
 import json
+import datetime
 import growattServer
+
 
 username = os.environ.get('SHINEPHONE_USERNAME', '')
 password = os.environ.get('SHINEPHONE_PASSWORD', '')
@@ -9,11 +11,16 @@ api = growattServer.GrowattApi()
 login_response = api.login(username, password)
 
 if login_response['success']:
+    createDateStr = login_response['user']['createDate']
+    startDate = datetime.datetime.fromisoformat(createDateStr).date()
+
     config = {
         'userId': login_response['userId'],
+        'startDate': startDate,
         'plantId': login_response['data'][0]['plantId'],
         'plantName': login_response['data'][0]['plantName'].strip()
     }
 
-    with open('../plant_data/config.json', 'w') as config_file:
+    with open('../config/config.json', 'w') as config_file:
         json.dump(config, config_file, indent=2)
+

d4dbe8b2fa38c48a9d3fb3f3fd9ad14427f38baf
Mike Dillamore <mike@dillamore.com>
2024-01-17 10:10:08 +0000

    Syntax fix and simplification

--- action_helpers/default_config.py
+++ action_helpers/default_config.py
@@ -1,6 +1,5 @@
 import os
 import json
-import datetime
 import growattServer
 
 
@@ -11,16 +10,12 @@ api = growattServer.GrowattApi()
 login_response = api.login(username, password)
 
 if login_response['success']:
-    createDateStr = login_response['user']['createDate']
-    startDate = datetime.datetime.fromisoformat(createDateStr).date()
-
     config = {
         'userId': login_response['userId'],
-        'startDate': startDate,
+        'startDate': login_response['user']['createDate'],
         'plantId': login_response['data'][0]['plantId'],
         'plantName': login_response['data'][0]['plantName'].strip()
     }
 
     with open('../config/config.json', 'w') as config_file:
         json.dump(config, config_file, indent=2)
-
